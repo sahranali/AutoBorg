ee399713798dc20377b7264c745e907bb9172eac
Omkar Londhe <49039825+Omkar47@users.noreply.github.com>
2019-09-04 11:09:21 +0530

    Rename ravana.py to ravanah.py

--- stdplugins/ravanah.py
+++ stdplugins/ravanah.py
@@ -32,15 +32,15 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "baalaji":
+    if input_str == "Ravana":
 
         await event.edit(input_str)
 
         animation_chars = [
         
-            "`Your bot is running\n\nTelethon version:` 1.9.0\n`Python:` 3.7.3\n`User:` @r4v4n4\n`Database Status: Databases functioning normally!`",
+            "`Your bot is running\n\nTelethon version:` 1.9.0\n`Python:` 3.7.3\n`User:` @iamomkarofficial\n`Database Status: Databases functioning normally!`",
             "`Connecting To github.com...`",
-            "`Deleting Baalaji Repo....`",
+            "`Deleting Ravana Repo....`",
             "`Forking Uniborg... 0%\n\n⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 0 MiB / 108.7 MiB`",
             "`Forking Uniborg... 4%\n\n⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 22 MiB / 108.7 MiB`",
             "`Forking Uniborg... 8%\n\n⬛️⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 48 MiB / 108.7 MiB`",    
@@ -49,8 +49,8 @@ async def _(event):
             "`Forking Uniborg... 52%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 90.7 MiB / 108.7 MiB `",
             "`Forking Uniborg... 84%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️\n\nFile Size: 100.7 MiB / 108.7 MiB `",
             "`Forking Uniborg... 100%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️\n\nFile Size: 108.7 MiB / 108.7 MiB\n\nTask Completed... `",
-            "`Fork Deploying...`\n\n@UniBorg ( `Custom Built By` @r4v4n4 ) \n`Verified Account:` ☑️\n`Official Website:` https://ravanaisdrunk.site.live\n\n`Python` `Loading...`\n[GCC 7.3.0]\n`Telethon` `Loading...`\n\n`Custom Built Fork:` `Loading...`",
-            "`Fork Deployed...`\n\n@UniBorg ( `Custom Built By` @r4v4n4 ) \n`Verified Account:` ✅\n`Official Website:` https://ravanaisdrunk.site.live\n\n`Python` 3.6.8 (default, Jan 29 2019, 19:35:16)\n[GCC 7.3.0]\n`Telethon` 1.8.0\n\n`Custom Built Fork:` https://github.com/ravana69/UniBorg"
+            "`Fork Deploying...`\n\n@UniBorg ( `Custom Built By` @iamomkarofficial ) \n`Verified Account:` ☑️\n`Official Website:` N/A\n\n`Python` `Loading...`\n[GCC 7.3.0]\n`Telethon` `Loading...`\n\n`Custom Built Fork:` `Loading...`",
+            "`Fork Deployed...`\n\n@UniBorg ( `Custom Built By` @iamomkaeofficial ) \n`Verified Account:` ✅\n`Official Website:` N/A\n\n`Python` 3.6.8 (default, Jan 29 2019, 19:35:16)\n[GCC 7.3.0]\n`Telethon` 1.8.0\n\n`Custom Built Fork:` https://github.com/Omkar47/Autoborg"
         ]
 
         for i in animation_ttl:
