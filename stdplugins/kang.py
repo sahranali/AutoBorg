546b7c11d489b0b979cb01578e881cb67bbb038f
Omkar Londhe <49039825+Omkar47@users.noreply.github.com>
2019-09-05 14:23:14 +0530

    Update kang.py

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -1,6 +1,6 @@
 """Make / Download Telegram Sticker Packs without installing Third Party applications
 Available Commands:
-.kang [Optional Emoji]
+.rom [Optional Emoji]
 .packinfo
 .getsticker"""
 from telethon import events
@@ -29,7 +29,7 @@ from telethon.tl.types import (
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("kang ?(.*)"))
+@borg.on(admin_cmd("rom ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
