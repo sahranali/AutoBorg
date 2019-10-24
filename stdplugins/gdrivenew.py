0ca96eb44e4d651b1c8c4fe4b7ee5fcb008c61fc
Omkar Londhe <49039825+Omkar47@users.noreply.github.com>
2019-10-24 21:21:29 +0530

    Update gdrivenew.py

--- stdplugins/gdrivenew.py
+++ stdplugins/gdrivenew.py
@@ -282,7 +282,7 @@ async def upload_file(http, file_path, file_name, mime_type, event, parent_id):
     media_body = MediaFileUpload(file_path, mimetype=mime_type, resumable=True)
     body = {
         "title": file_name,
-        "description": "Uploaded using @UniBorg gDrive v2",
+        "description": "Uploaded using @AutoBorg gDrive v2",
         "mimeType": mime_type,
     }
     if parent_id is not None:
