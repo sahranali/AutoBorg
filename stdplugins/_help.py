0ec595623da1c01ca13e28c8e309a5638a7a0247
Omkar Londhe <49039825+Omkar47@users.noreply.github.com>
2019-10-24 22:24:32 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -54,7 +54,7 @@ async def _(event):
     result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
     result = result.stringify()
     logger.info(result)  # pylint:disable=E0602
-    await event.edit("""Telethon UserBot powered by @UniBorg""")
+    await event.edit("""Telethon UserBot powered by @AutoBorg""")
     
     
 @borg.on(admin_cmd(pattern="autoborg (.*)"))
